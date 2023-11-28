d4dabe124e3273b4b0f7d67c95796103c5cc169c
Beksultan Mamashov <ironman.beks@gmail.com>
2023-11-28 11:25:37 +0600

    chore: CatalogInteriorDoors consts

--- src/components/pages/CatalogPage/CatalogInteriorDoorsPage/const.tsx
+++ src/components/pages/CatalogPage/CatalogInteriorDoorsPage/const.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
+import {
+    TCategoriesFormDefaultValues,
+    TSortFormDefaultValues,
+    TTagsFormDefaultValues,
+} from "./types";
 
-export const sortTypes = [
-    { text: "By popularity" },
+const sortTypes = [
+    { text: "By popularity", value: "by_popularity" },
     {
         text: "Name",
+        value: "name",
         icon: (
             <Image
                 alt="sort by name"
@@ -12,6 +18,49 @@ export const sortTypes = [
                 height={24}
             />
         ),
+        descIcon: (
+            <Image
+                alt="sort by name"
+                src="/icons/sort-z-a.svg"
+                width={24}
+                height={24}
+            />
+        ),
     },
-    { text: "Price" },
+    { text: "Price", value: "price" },
 ];
+
+// Form Default Values
+const sortFormDefaultValues: TSortFormDefaultValues = {
+    sort: "",
+};
+
+const tagsFormDefaultValues: TTagsFormDefaultValues = {
+    tags: [],
+};
+
+const SLIDER_STEPS = 100;
+const MAX_PRICE = 10000;
+const MIN_PRICE = 0;
+const categoriesFormDefaultValues: Omit<
+    TCategoriesFormDefaultValues,
+    "door_modification"
+> = {
+    eyelet_included: [],
+    price: [MIN_PRICE, MAX_PRICE],
+    categories: {
+        doors: [],
+        interior_doors: [],
+        panel_header_3: [],
+    },
+};
+
+export {
+    sortFormDefaultValues,
+    sortTypes,
+    tagsFormDefaultValues,
+    categoriesFormDefaultValues,
+    SLIDER_STEPS,
+    MAX_PRICE,
+    MIN_PRICE,
+};
