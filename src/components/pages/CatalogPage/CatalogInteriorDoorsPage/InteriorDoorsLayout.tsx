df04e84e3d6ed3137243bb2ae8e9b42df91e1d92
Beksultan Mamashov <ironman.beks@gmail.com>
2023-11-28 11:25:55 +0600

    feat: CatalogInteriorDoors Layout Extended

--- src/components/pages/CatalogPage/CatalogInteriorDoorsPage/InteriorDoorsLayout.tsx
+++ src/components/pages/CatalogPage/CatalogInteriorDoorsPage/InteriorDoorsLayout.tsx
@@ -2,12 +2,17 @@ import { H2 } from "@components/Text";
 import Breadcrumb from "@components/globalComponents/Breadcrumb";
 import { TStore } from "@globalTypes/storeTypes";
 import { inject, observer } from "mobx-react";
-import { FC, ReactNode } from "react";
-import { tags } from "./FiltersMockUp";
+import { FC, ReactNode, useEffect } from "react";
+import { useRouter } from "next/router";
+import { Controller, FormProvider, useForm } from "react-hook-form";
 import Tag from "@components/Tag";
 
 import LeftSide from "./FiltersSide";
 import RightSide from "./ContentSide";
+import { addSearchQueryParams } from "./helper/addSearchQueryParams";
+import { TTagsFormDefaultValues } from "./types";
+import { tagsFormDefaultValues } from "./const";
+import { tags } from "./FiltersMockUp";
 
 interface InteriorDoorsLayoutProps extends TStore {
     pageClassPrefix: string;
@@ -21,6 +26,46 @@ const InteriorDoorsLayout: FC<
     } & InteriorDoorsLayoutProps
 > = inject("store")(
     observer(({ pageClassPrefix }) => {
+        const router = useRouter();
+        const defaultTags = router.query.tags as string;
+        const methods = useForm<TTagsFormDefaultValues>({
+            defaultValues: tagsFormDefaultValues,
+        });
+        const {
+            control,
+            watch,
+            setValue,
+            formState: { isDirty },
+        } = methods;
+
+        const formTagsValue = watch("tags");
+
+        useEffect(() => {
+            if (isDirty) {
+                const filteredTags = formTagsValue.filter(Boolean);
+
+                addSearchQueryParams(router, {
+                    tags: filteredTags.length
+                        ? filteredTags.join(",")
+                        : undefined,
+                });
+            }
+        }, [formTagsValue.join(","), isDirty]);
+
+        useEffect(() => {
+            const modifedDefaultTags = tags.map(({ value }) => {
+                const isInArray = defaultTags
+                    ?.split(",")
+                    .find((queryTag) => queryTag === value);
+
+                if (isInArray) return value;
+
+                return null;
+            });
+
+            if (defaultTags) setValue("tags", modifedDefaultTags);
+        }, [defaultTags]);
+
         return (
             <div>
                 <Breadcrumb />
@@ -30,13 +75,35 @@ const InteriorDoorsLayout: FC<
                         91 goods
                     </span>
                 </H2>
-                <div className={`${pageClassPrefix}_filter-tags`}>
-                    {tags.map((key) => (
-                        <Tag size="small" isActive>
-                            {key}
-                        </Tag>
-                    ))}
-                </div>
+                <FormProvider {...methods}>
+                    <div className={`${pageClassPrefix}_filter-tags`}>
+                        {tags.map(({ text, value: tagValue }, index) => (
+                            <Controller
+                                control={control}
+                                name={`tags.${index}`}
+                                key={tagValue}
+                                render={({ field: { onChange, value } }) => {
+                                    const onClick = () => onChange(tagValue);
+                                    const onCloseIconClick = () =>
+                                        onChange(null);
+                                    const isActive = tagValue === value;
+                                    return (
+                                        <Tag
+                                            size="small"
+                                            onClick={onClick}
+                                            isCloseIcon={isActive}
+                                            onCloseIconClick={onCloseIconClick}
+                                            isActive={isActive}
+                                        >
+                                            {text}
+                                        </Tag>
+                                    );
+                                }}
+                            />
+                        ))}
+                    </div>
+                </FormProvider>
+
                 <div className={`${pageClassPrefix}_content`}>
                     <div className={`${pageClassPrefix}_left-side`}>
                         <LeftSide pageClassPrefix={pageClassPrefix} />
